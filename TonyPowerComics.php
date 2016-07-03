cb5e17227281c9f53385ecf18b1bebd93278c8ae
Hazelnut20G <Gavinhays@hotmail.com.au>
2016-07-03 17:40:47 +1000

    Add files via upload

---
+++ TonyPowerComics.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<head>
+
+<title>Tony Power Comics</title>
+
+
+</head>
+
+<body>
+	<p>
+      <?php
+        $fruits = array('bananas', 'apples', 'pears');
+        echo 'I love eating ' . $fruits[1] . ' too!';
+      ?>
+      
+    </p>
+
+
+
+
+</body>
+
+<footer>
+
+
+
+</footer>
+
+</html>
